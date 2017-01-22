a29f51aae7bfa9fbe99eecf4d5eb636f0b13dc11
Glen Rodgers <glenmrodgers@gmail.com>
2017-01-22 14:35:29 +0000

    Created the basic tests for a github user and a github repo

--- src/test/GithubRepoTest.java
+++ src/test/GithubRepoTest.java
@@ -1,5 +1,42 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.GithubRepo;
+
 public class GithubRepoTest {
+	
+    private static final String GITHUB_OWNER = "gmrodgers";
+    private static final String GITHUB_REPO = "Enigma_machine";
+    private GithubRepo repo;
+    
+    @Before
+    public void setUp() {
+    	repo = new GithubRepo(GITHUB_OWNER, GITHUB_REPO);
+    }
+    
+	@Test
+	public void getLanguageCountsTest() {
+		Map<String, Integer> counts = repo.getLanguageCounts();
+		assertEquals((int) counts.get("C++"), 6271);
+		assertEquals((int) counts.get("Makefile"), 334);
+		assertEquals((int) counts.get("PureBasic"), 167);
+		assertEquals((int) counts.get("Python"), 6793);
+	}
+	
+	@Test
+	public void getNameTest() {
+		assertEquals(GITHUB_REPO, repo.getRepoName());
+	}
+	
+	@Test
+	public void getOwner() {
+		assertEquals(GITHUB_REPO, repo.getOwner());
+	}
 
 }
--- src/test/GithubUserTest.java
+++ src/test/GithubUserTest.java
@@ -1,5 +1,41 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import main.GithubRepo;
+import main.GithubUser;
+
 public class GithubUserTest {
 	
+    private static final String GITHUB_USER = "gmrodgers";
+    private static final String[] GITHUB_REPOS = {
+    		"Enigma_machine", "Sorting_comparisons", "WACC_compiler", "webapps_2016"
+    };
+    private GithubUser user;
+    private List<String> githubRepos;
+    
+    @Before
+    public void setUp() {
+    	user = new GithubUser(GITHUB_USER);
+    	githubRepos = new LinkedList<String>(Arrays.asList(GITHUB_REPOS));
+    }
+    
+	@Test
+	public void getReposTest()  {
+		List<GithubRepo> repos = user.getRepos();
+		ListIterator<GithubRepo> lit = repos.listIterator();
+		while (lit.hasNext()) {
+			GithubRepo repo = (GithubRepo) lit.next();
+			assertTrue(githubRepos.contains(repo.getRepoName()));
+		}
+	}
+
 }
