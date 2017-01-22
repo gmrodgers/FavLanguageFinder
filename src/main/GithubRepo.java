abd64ca9e0e2cc8cb4d581efa284d6f31d02fa94
Glen Rodgers <glenmrodgers@gmail.com>
2017-01-22 14:35:46 +0000

    The autogenerated stubs to make the sure the tests compile

--- src/main/GithubRepo.java
+++ src/main/GithubRepo.java
@@ -1,5 +1,26 @@
 package main;
 
+import java.util.Map;
+
 public class GithubRepo {
+
+	public GithubRepo(String githubOwner, String githubRepo) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Map<String, Integer> getLanguageCounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getRepoName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getOwner() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/GithubUser.java
+++ src/main/GithubUser.java
@@ -1,5 +1,16 @@
 package main;
 
+import java.util.List;
+
 public class GithubUser {
+
+	public GithubUser(String githubUser) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<GithubRepo> getRepos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
