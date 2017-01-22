f7558c8ab2cd023882b14a6d0c63c7c5bf2cbdfa
Glen Rodgers <glenmrodgers@gmail.com>
2017-01-22 14:41:56 +0000

    Created the main function and added test cases for utility methods used

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,29 @@
 package main;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Please enter a user of github:");
+		Scanner input = new Scanner(System.in);
+		String user = input.nextLine();
+		GithubUser githubUser = new GithubUser(user);
+		input.close();
+		Map<String, Integer> totals = calculateLanguageTotals(githubUser.getRepos());
+		System.out.println(user + "'s favourite programming language is " + maxTotal(totals));
+	}
 
+	public static String maxTotal(Map<String, Integer> totals) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Map<String, Integer> calculateLanguageTotals(List<GithubRepo> repos) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
---
+++ src/test/MainTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import main.GithubRepo;
+import main.Main;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+	
+	private Map<String, Integer> totals;
+	private List<GithubRepo> repos;
+	private static final GithubRepo[] GITHUB_REPOS = {
+			new GithubRepo("gmrodgers", "Enigma_machine"),
+			new GithubRepo("gmrodgers", "Sorting_comparisons")
+	};
+	
+	@Before
+	public void setUp() {
+		totals = new HashMap<>();
+		repos = new LinkedList<>(Arrays.asList(GITHUB_REPOS));
+	}
+
+	@Test
+	public void maxTotalFindsCorrectLanguageTest() {
+		totals.put("Java", 19);
+		totals.put("Javascript", 20);
+		totals.put("Haskell", 18);
+		assertEquals(Main.maxTotal(totals), "Javascript");
+	}
+	
+	@Test
+	public void calculateLanguageTotalsTest() {
+		Map<String, Integer> newTotals = Main.calculateLanguageTotals(repos);
+		assertEquals((int) newTotals.get("C++"), 6271);
+		assertEquals((int) newTotals.get("Makefile"), 334);
+		assertEquals((int) newTotals.get("PureBasic"), 167);
+		assertEquals((int) newTotals.get("Python"), 6793);
+		assertEquals((int) newTotals.get("Jupyter Notebook"), 87690);
+		
+		
+	}
+
+}
